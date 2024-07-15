737fc2325952689d2d6eb0b4d226ece41ebf4ad6
wamalaabdul <kanyesigyenathan8@gmail.com>
2024-07-15 19:25:28 +0300

    finally on firebase

--- agrotrustapp/lib/main.dart
+++ agrotrustapp/lib/main.dart
@@ -1,6 +1,9 @@
 
 
+import 'dart:io';
+
 import 'package:agrotrustapp/home.dart';
+import 'package:agrotrustapp/product.dart';
 //import 'package:agrotrustapp/login.dart';
 
  
@@ -13,9 +16,17 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
-  runApp(MyApp());
+Platform.isAndroid?
+await Firebase.initializeApp(
+  options: const FirebaseOptions(
+   apiKey: "AIzaSyBWIjZEksSC2ATG5gpF_Kq7_67t",
+   appId: "1:258868804337:android:367fa7057204c7dc178750",
+   messagingSenderId: "258868804337",
+   projectId: "agrotrust-18dc8") 
+  )
+    
+    :await Firebase.initializeApp();
+    runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
