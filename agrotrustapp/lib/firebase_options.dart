9b0986c119140a85590290cadf49cbd8b1a2e5ef
hasahyasharif <hasahyasharif@gmail.com>
2024-07-26 13:39:42 +0300

    wamz updates

--- agrotrustapp/lib/firebase_options.dart
+++ agrotrustapp/lib/firebase_options.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    return FirebaseOptions(
+    return const FirebaseOptions(
       apiKey: "AIzaSyBWIjZEksSC2ATG5gpF_Kq7_67tVQYZIAA",
       appId: "1:258868804337:android:367fa7057204c7dc178750",
       messagingSenderId: "258868804337",
