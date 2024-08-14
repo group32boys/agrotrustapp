330b352b96731e9704f52de9ef66025d97e2756d
Kipaalu Derrick <kipaaluderrick49@gmail.com>
2024-08-14 11:59:49 +0300

    readme

--- agrotrustapp/lib/services/firebase_service.dart
+++ agrotrustapp/lib/services/firebase_service.dart
@@ -1,21 +1,27 @@
- import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:agrotrustapp/models/seller.dart';
 import 'package:agrotrustapp/models/product_model.dart';
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 
 class FirebaseService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final FirebaseStorage _storage = FirebaseStorage.instance;
 
   // Fetch sellers
   Future<List<Seller>> fetchSellers() async {
     final snapshot = await _firestore.collection('sellers').get();
     return snapshot.docs.map((doc) {
+      doc.data();
       return Seller.fromDocument(doc);
     }).toList();
+  }
+
+  // Fetch a single seller by ID
+  Future<Seller?> fetchSellerById(String sellerId) async {
+    final sellerDoc = await _firestore.collection('sellers').doc(sellerId).get();
+    if (sellerDoc.exists) {
+      return Seller.fromDocument(sellerDoc);
+    } else {
+      return null;
+    }
   }
 
   // Fetch products for a seller
@@ -88,40 +94,6 @@ class FirebaseService {
         'numberOfRatings': 1,
         'feedback': [feedback],
       });
-    }
-  }
-
-  // Upload a profile picture for a seller
-  Future<void> uploadProfilePicture(String filePath, String sellerId) async {
-    File file = File(filePath);
-    try {
-      // Upload file to Firebase Storage
-      await _storage.ref('profile_pictures/$sellerId').putFile(file);
-
-      // Get the download URL
-      String downloadURL = await _storage.ref('profile_pictures/$sellerId').getDownloadURL();
-
-      // Update the Firestore document with the download URL
-      await _firestore.collection('sellers').doc(sellerId).update({
-        'profilePictureUrl': downloadURL,
-      });
-    } on FirebaseException catch (e) {
-      if (kDebugMode) {
-        print('Failed to upload and update profile picture: ${e.message}');
-      }
-    }
-  }
-
-  // Retrieve profile picture URL
-  Future<String?> getProfilePictureUrl(String sellerId) async {
-    try {
-      String downloadURL = await _storage.ref('profile_pictures/$sellerId').getDownloadURL();
-      return downloadURL;
-    } catch (e) {
-      if (kDebugMode) {
-        print('Failed to retrieve profile picture URL: $e');
-      }
-      return null;
     }
   }
 }
